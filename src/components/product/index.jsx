06659c59a648bebc32d9826d3552625068ba6fcf
AqibNiazi <aqibjaved5201@gmail.com>
2023-11-19 20:27:30 +0500

    adding and removing products from the cart logic completed

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -25,27 +25,64 @@ const Product = () => {
     dispatch(add(product));
   };
 
+  const renderStarRating = (rating) => {
+    const numberOfStars = 5;
+    const filledStars = Math.floor(rating);
+    const starElements = [];
+
+    for (let i = 0; i < numberOfStars; i++) {
+      if (i < filledStars) {
+        // Filled star
+        starElements.push(
+          <svg
+            key={i}
+            aria-hidden="true"
+            className="h-5 w-5 text-yellow-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+          </svg>
+        );
+      } else {
+        // Unfilled star
+        starElements.push(
+          <svg
+            key={i}
+            aria-hidden="true"
+            className="h-5 w-5 text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+          </svg>
+        );
+      }
+    }
+    return starElements;
+  };
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
       {products.map((product) => {
         return (
           <div className="mx-auto relative m-10 w-full max-w-xs overflow-hidden rounded-lg bg-white shadow-md shadow-gray-400 ">
-            <a href="#">
-              <img
-                className="mx-auto h-60 rounded-t-lg object-cover"
-                src={product?.image}
-                alt="product image"
-              />
-            </a>
+            <img
+              className="mx-auto h-60 rounded-t-lg object-cover"
+              src={product?.image}
+              alt="product image"
+              title={product.description}
+            />
+
             <span className="absolute top-0 left-0 w-28 translate-y-4 -translate-x-6 -rotate-45 bg-black text-center text-sm text-white">
               Sale
             </span>
             <div className="mt-4 px-5 pb-5">
-              <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-slate-900">
-                  Nike Air MX Super 5000
-                </h5>
-              </a>
+              <h5 className="text-xl font-semibold tracking-tight text-slate-900">
+                {product.title}
+              </h5>
+
               <div className="mt-2.5 mb-5 flex items-center">
                 <span className="mr-2 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
                   {product.rating.rate}
@@ -103,7 +140,6 @@ const Product = () => {
                   </span>
                 </p>
                 <button
-                  href="#"
                   className="flex items-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
                   onClick={() => addProducts(product)}
                 >
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -6,15 +6,35 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     add(state, action) {
-      //   In redux we can't directly mutate state
-      //   return [...state, action.payload];
-      //    In Redux-toolkit we can do that
-      state.push(action.payload);
+      const existingProductIndex = state.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingProductIndex !== -1) {
+        // If the product already exists in the cart, update its quantity
+        state[existingProductIndex].quantity += 1;
+      } else {
+        // If it's a new product, add it to the cart
+        state.push({ ...action.payload, quantity: 1 });
+      }
     },
     remove(state, action) {
-      state.filter((item) => item.id !== action.payload);
+      return state.filter((item) => item.id !== action.payload);
+    },
+    increaseProductQuantity(state, action) {
+      const product = state.find((item) => item.id === action.payload);
+      if (product) {
+        product.quantity += 1;
+      }
+    },
+    decreaseProductQuantity(state, action) {
+      const product = state.find((item) => item.id === action.payload);
+      if (product && product.quantity > 1) {
+        product.quantity -= 1;
+      }
     },
   },
 });
-export const { add, remove } = cartSlice.actions;
+export const { add, remove, increaseProductQuantity, decreaseProductQuantity } =
+  cartSlice.actions;
 export default cartSlice.reducer;
