593d276e2cf850cae9914009ae68db1536f1e7a2
AqibNiazi <aqibjaved5201@gmail.com>
2023-12-22 00:02:36 +0500

    practicing Material UI

---
+++ src/components/navbar/NavLink.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavLink = ({ to, text, darkMode }) => {
+  return (
+    <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-color md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+      <Link
+        to={to}
+        className={`block py-2 px-3 rounded md:bg-transparent text-black md:hover:text-color md:p-0 ${
+          darkMode ? "dark:text-white md:dark:text-blue-500" : ""
+        }`}
+        aria-current="page"
+      >
+        {text}
+      </Link>
+    </li>
+  );
+};
+
+export default NavLink;
