4523345dce9bb0989b1446269150ba6bc41353bf
AqibNiazi <aqibjaved5201@gmail.com>
2024-01-19 00:21:57 +0500

    light dark mode button added

--- src/components/navbar/NavLink.jsx
+++ src/components/navbar/NavLink.jsx
@@ -6,9 +6,6 @@ const NavLink = ({ to, text, darkMode }) => {
     <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-color md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
       <Link
         to={to}
-        className={`block py-2 px-3 rounded md:bg-transparent text-black md:hover:text-color md:p-0 ${
-          darkMode ? "dark:text-white md:dark:text-blue-500" : ""
-        }`}
         aria-current="page"
       >
         {text}
